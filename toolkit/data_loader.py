f46c0e9e87b07da31f0c6b3aa0b1103d63dc5313
Raman1121 <happy07.dutt@gmail.com>
2025-04-24 13:50:02 +0100

    mod

--- toolkit/data_loader.py
+++ toolkit/data_loader.py
@@ -644,6 +644,9 @@ def get_dataloader_from_datasets(
     else:
         dataloader_kwargs['num_workers'] = dataset_config_list[0].num_workers
         dataloader_kwargs['prefetch_factor'] = dataset_config_list[0].prefetch_factor
+    
+    # Manually setting dataloaders to 4
+    dataloader_kwargs['num_workers'] = 4
 
     print("###################################")
     print("##### NUM WORKERS: ", dataloader_kwargs['num_workers'])
